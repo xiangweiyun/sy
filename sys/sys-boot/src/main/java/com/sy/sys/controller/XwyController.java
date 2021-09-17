f8a193bbcec582d616f6616d20ad4acb0d99333e
“ <xiangwy1029@163.com>
2021-09-17 13:50:05 +0800

    项目完善

--- sys/sys-boot/src/main/java/com/sy/sys/controller/XwyController.java
+++ sys/sys-boot/src/main/java/com/sy/sys/controller/XwyController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class XwyController {
 	private final Logger logger = LoggerFactory.getLogger(XwyController.class);
 
-	@DubboReference
+	@DubboReference(version = "${dubbo.service.version}")
 	private XwyService xwyService;
 
 	@GetMapping("/getName")
