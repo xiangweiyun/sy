a5a25663abb13dbb794290ff3adf6c192598cbd8
“ <xiangwy1029@163.com>
2021-09-17 18:14:34 +0800

    项目完善

--- center/center-common/src/main/java/com/sy/center/common/base/BaseEntity.java
+++ center/center-common/src/main/java/com/sy/center/common/base/BaseEntity.java
@@ -9,7 +9,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Column;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -43,29 +42,24 @@ public class BaseEntity implements Serializable {
 	public static final String LAST_MODIFIED_BY = "lastModifiedBy";
 
 	@Id
-	@Column(name = "ID")
 	@ApiModelProperty(value = "主键")
 	@TableId(value = "ID", type = IdType.ID_WORKER)
 	private Long id;
 
-	@Column(name = "CREATE_BY", updatable = false)
 	@ApiModelProperty(value = "创建人")
 	@TableField(value = "CREATE_BY", fill = FieldFill.INSERT)
 	private Long createBy;
 
-	@Column(name = "CREATED_DATE", updatable = false)
 	@ApiModelProperty(value = "创建时间")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	@TableField(value = "CREATE_DATE", fill = FieldFill.INSERT, update = "now()")
 	private LocalDateTime createDate;
 
-	@Column(name = "LAST_MODIFIED_DATE")
 	@ApiModelProperty(value = "更新时间")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	@TableField(value = "LAST_MODIFIED_DATE", fill = FieldFill.INSERT_UPDATE, update = "now()")
 	private LocalDateTime lastModifiedDate;
 
-	@Column(name = "LAST_MODIFIED_BY")
 	@ApiModelProperty(value = "更新人")
 	@TableField(value = "LAST_MODIFIED_BY", fill = FieldFill.INSERT_UPDATE)
 	private Long lastModifiedBy;
