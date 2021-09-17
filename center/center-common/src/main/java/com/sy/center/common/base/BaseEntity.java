00b6cb3bc7dd61ec8458ecb06c40a534630f50cc
“ <xiangwy1029@163.com>
2021-09-17 18:34:59 +0800

    项目完善

--- center/center-common/src/main/java/com/sy/center/common/base/BaseEntity.java
+++ center/center-common/src/main/java/com/sy/center/common/base/BaseEntity.java
@@ -47,13 +47,13 @@ public class BaseEntity implements Serializable {
 	private Long id;
 
 	@ApiModelProperty(value = "创建人")
-	@TableField(value = "CREATE_BY", fill = FieldFill.INSERT)
-	private Long createBy;
+	@TableField(value = "CREATED_BY", fill = FieldFill.INSERT)
+	private Long createdBy;
 
 	@ApiModelProperty(value = "创建时间")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	@TableField(value = "CREATE_DATE", fill = FieldFill.INSERT, update = "now()")
-	private LocalDateTime createDate;
+	@TableField(value = "CREATED_DATE", fill = FieldFill.INSERT, update = "now()")
+	private LocalDateTime createdDate;
 
 	@ApiModelProperty(value = "更新时间")
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
