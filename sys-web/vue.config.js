0d94a9b108b3133399e3ebe7556ad7adde554cc1
“ <xiangwy1029@163.com>
2021-09-28 17:59:31 +0800

    项目完善

--- sys-web/vue.config.js
+++ sys-web/vue.config.js
@@ -17,7 +17,7 @@ function resolve(dir) {
 
 module.exports = {
   publicPath: './', // 部署应用包时的基本 url
-  outputDir: 'sys-web', // build 构建文件目录
+  outputDir: 'dist', // build 构建文件目录
   assetsDir: 'static', // 静态资源目录
   lintOnSave: process.env.NODE_ENV === 'development', // 仅在开发模式下进行 eslint 检测代码
   productionSourceMap: false, // 禁用生产环境的 source map
